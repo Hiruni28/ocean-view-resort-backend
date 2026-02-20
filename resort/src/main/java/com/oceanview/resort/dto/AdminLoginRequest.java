66382d7d864371e6dfa737bbf5ca54d081873d85
Hiruni28 <hiruniparindhya28@gmail.com>
2026-02-20 13:15:34 +0530

    Add AdminLoginRequest DTO

---
+++ resort/src/main/java/com/oceanview/resort/dto/AdminLoginRequest.java
@@ -0,0 +1,12 @@
+package com.oceanview.resort.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AdminLoginRequest {
+    private String username;
+    private String password;
+
+}
