5541f22a9ed7ba95a556916be8c00cb0a0ebcee5
Hiruni28 <hiruniparindhya28@gmail.com>
2026-02-20 13:19:13 +0530

    Add CustomerRepository

---
+++ resort/src/main/java/com/oceanview/resort/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.oceanview.resort.repository;
+import com.oceanview.resort.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String email);
+}
+
