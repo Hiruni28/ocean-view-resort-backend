e1d1ba421718f2fdf32b946594f8700ab4b0be77
Hiruni28 <hiruniparindhya28@gmail.com>
2026-02-20 13:17:14 +0530

    Add Customer entity model

---
+++ resort/src/main/java/com/oceanview/resort/model/Customer.java
@@ -0,0 +1,26 @@
+package com.oceanview.resort.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+    private String contact;
+    private String NIC;
+    private String email;
+    private String password;
+
+
+}
