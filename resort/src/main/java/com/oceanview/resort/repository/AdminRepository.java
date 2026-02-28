a2c3e8d5296acc47c15d28fad8b1673c47f83d56
Hiruni28 <hiruniparindhya28@gmail.com>
2026-02-28 12:06:34 +0530

    Updated Admin Repository

--- resort/src/main/java/com/oceanview/resort/repository/AdminRepository.java
+++ resort/src/main/java/com/oceanview/resort/repository/AdminRepository.java
@@ -4,8 +4,12 @@ import com.oceanview.resort.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface AdminRepository extends JpaRepository<Admin,Long> {
-    Admin findByUsernameAndPassword(String u,String p);
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    // (Optional) Used only if you want JPA login
+    Admin findByUsernameAndPassword(String username, String password);
+
+    // For JPA lookup by username
+    Admin findByUsername(String username);
 }
